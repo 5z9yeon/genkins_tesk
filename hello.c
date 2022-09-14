470462ba7c49f28b702b6decca7f4206daaa81fb
IOM9 <113456069+IOM9@users.noreply.github.com>
2022-09-14 09:55:45 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,13 +2,13 @@
 #include "test.h"
 
 int main() {
+  aaaaaaaaaaaaaaaa
+  
   printf("Hello!");
   printf("World!");
   
   printf("join IOM!");
   
   printf(sum(1,2));
-  pritnf(sss());
-  
   
 }
